b5837eec2fc4667f532d62cc56f12f945a3f9223
Diego Fernando Pertuz <diegofoxfire@gmail.com>
2025-05-18 20:24:53 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+def saludar(nombre): 
+    print("Hola " + nombre) 
+ 
+def dividir(a, b): 
+    return a / b 
+ 
+saludar("Mundo") 
+print(dividir(10, 0)) 
+ 
---
+++ utilidades.py
@@ -0,0 +1,17 @@
+def sumar(a, b): 
+    return a + b 
+ 
+def restar(a, b): 
+    return a - b 
+def multiplicar(a, b): 
+    return a * b 
+ 
+def dividir(a, b): 
+    if b == 0: 
+        return "Error: división por cero" 
+    return a / b 
+def operacion_innecesaria(x): 
+    if x > 0: 
+        return True 
+    else: 
+        return True 
